b939be2ede8e4398fd18126af98fdbf0890fe7be
camachoRamel <142206766+camachoRamel@users.noreply.github.com>
2025-04-06 22:30:51 +0800

    added all other templates needed

--- views/signup/index.php
+++ views/signup/index.php
@@ -25,47 +25,70 @@ if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])){
     
     <div class="container mt-3"><h1 class="text-center">Sign up form</h1></div>
     
-    <div class="container rounded bg-white">
+    <div class="container mb-4 rounded bg-white shadow">
         <div class="justify-content-center p-4 mt-3">
 
        <form action="" method="POST">
-            <!-- <div class="container "> -->
                 <div class="form-group has-validation">
-                    <label for="username">Username:</label>
-                    <input class="form-control" type="text" name="username" id="username" required>
+                    <label class="font-weight-bold" for="username">Username:</label>
+                    <input class="form-control" type="text" name="username" required>
                     <!-- <div class="d-inline text-danger">Username can't be empty.</div> -->
                 </div>
                 <div class="form-group">
-                    <label for="email">Email:</label>
+                    <label class="font-weight-bold" for="email">Email:</label>
                     <input class="form-control" type="email" name="email" required>
                 </div>
                 <div class="form-group">
-                    <label for="password">Password:</label>
+                    <label class="font-weight-bold" for="password">Password:</label>
                     <input class="form-control" type="password" name="password" required>
                 </div>
                 <div class="form-group">
-                    <label for="confirm-password">Confirim Password:</label>
+                    <label class="font-weight-bold" for="confirm-password">Confirim Password:</label>
                     <input class="form-control" type="password" name="confirm-password" required>
                 </div>
                 <div class="form-group">
-                    <label for="age">Age:</label>
+                    <label class="font-weight-bold" for="age">Age:</label>
                     <input class="form-control" type="number" name="age" required>
                 </div>
                 <div class="form-group">
-                    <label for="gender">Gender:</label>
+                    <label class="font-weight-bold" for="gender">Gender:</label>
                     <select class="form-control" name="gender" required>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select>
                 </div>
                 <div class="text-center w-100">
-                <button class="btn btn-success mt-3 w-100" type="submit" name="submit" value="submit">Sign up</button>
-                <!-- </div> -->
+                <button class="btn btn-success font-weight-bold mt-3 w-100" type="submit" name="submit" value="submit">Sign up</button>
             </div>
        </form> 
         </div>
     </div>
 
+
+    <!-- REGISTRATION SUCCESSFUL -->
+    <div class="container rounded bg-white shadow mb-4">
+        <div class="container"><h2 class="font-weight-bold py-4">Registration Successful!</h2></div>
+
+        <div class="container">
+            <span class="d-block font-weight-bold">Username: <p class="d-inline font-weight-normal">THis is username</p></span>
+            <span class="d-block font-weight-bold">Email: <p class="d-inline font-weight-normal">Email</p></span>
+            <span class="d-block font-weight-bold">Age: <p class="d-inline font-weight-normal">Age</p></span>
+            <span class="d-block font-weight-bold">Gender: <p class="d-inline font-weight-normal">Gender</p></span>
+            <span class="d-block font-weight-bold">Request Method: <p class="d-inline font-weight-normal"><?= $_SERVER['REQUEST_METHOD'] ?></p></span>
+            <span class="d-block font-weight-bold">Script Name: <p class="d-inline font-weight-normal"><?= $_SERVER['PHP_SELF'] ?></p></span>
+        </div>
+
+    </div>
+
+    <!-- WELCOME USER -->
+    <div class="container mb-3">
+        <p class="text-center">Welcome back, <span class="d-inline font-weight-bold">User</span></p>
+    </div>
+
+    <!-- LAST USED EMAIL -->
+    <div class="container mb-3">
+        <p class="text-center">Your last used email, <span class="d-inline font-weight-bold">Email</span></p>
+    </div>
 
 </body>
 </html>
